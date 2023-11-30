7d0d0ce985890a786d36c88fa5e8cd453244fb10
Josué Juca <josuejuca19@gmail.com>
2023-11-29 22:54:05 -0300

    Arquivo de direcionamento.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta http-equiv="refresh" content="0; url=app/views/login.php">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login PHP</title>
+</head>
+
+<body>
+    <?php
+    echo "Direcionando";
+    ?>
+</body>
+
+</html>
